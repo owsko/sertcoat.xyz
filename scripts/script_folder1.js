8002f38caf2d935de5e64adfec7beeb1637195ab
dex <dex.personal@pm.me>
2024-07-05 17:44:51 +0300

    Changes to be committed: 	new file:   .vscode/launch.json 	new file:   .vscode/settings.json 	new file:   contact.html 	new file:   index.html 	new file:   media/Static_02.png 	new file:   media/Static_20.jpg 	new file:   media/Static_22.JPG 	new file:   media/Static_23.JPG 	new file:   pages/empty.html 	new file:   pages/files/file1.html 	new file:   pages/files/file2.html 	new file:   pages/folders/folder1.html 	new file:   pages/folders/folder2.html 	new file:   scripts/script_file.js 	new file:   scripts/script_folder1.js 	new file:   scripts/script_randomHeader.js 	new file:   stylesheets/commonStyles.css 	new file:   stylesheets/stylesheet.css 	new file:   stylesheets/subStyle.css

---
+++ scripts/script_folder1.js
@@ -0,0 +1,38 @@
+let folderSelecEl = document.getElementById("iframe-folder");
+//testAgainst.addEventListener("click", display_folder1());
+
+function display_folder1() {
+    folderSelecEl.src = "pages/folders/folder1.html"
+}
+function display_folder2() {
+    folderSelecEl.src = "pages/folders/folder2.html"
+}
+function display_folder3() {
+    folderSelecEl.src = "pages/empty.html"
+}
+function display_folder4() {
+    folderSelecEl.src = "pages/empty.html"
+}
+function display_folder5() {
+    folderSelecEl.src = "pages/empty.html"
+}
+function display_folder6() {
+    folderSelecEl.src = "pages/empty.html"
+}
+function display_folder7() {
+    folderSelecEl.src = "pages/empty.html"
+}
+function display_folder8() {
+    folderSelecEl.src = "pages/empty.html"
+}
+function display_folder9() {
+    folderSelecEl.src = "pages/empty.html"
+}
+
+
+
+
+
+
+
+
---
+++ scripts/script_randomHeader.js
@@ -0,0 +1,10 @@
+let headerEl = document.getElementById("header-h1");
+const emoticon = ["༼ ༎ຶ ᆺ ༎ຶ༽", "◑____________◑", "(,,◕　⋏　◕,,)"];
+
+function randomEmoticon() {
+    let i = Math.floor(Math.random() * emoticon.length);
+    headerEl.textContent = emoticon[i];
+}
+
+randomEmoticon();
+
