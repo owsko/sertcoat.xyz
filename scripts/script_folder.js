1cf7e04024d5cedc08bc1b8f176014abf729cd63
owsko <owslo.pm@gmail.com>
2024-07-07 00:25:46 +0300

    menu bar

--- scripts/script_folder.js
+++ scripts/script_folder.js
@@ -2,7 +2,7 @@ let folderSelecEl = document.getElementById("iframe-folder");
 //testAgainst.addEventListener("click", display_folder1());
 
 function display_folder1() {
-    folderSelecEl.src = "pages/empty.html"
+    folderSelecEl.src = "pages/folders/folder1.html"
 }
 function display_folder2() {
     folderSelecEl.src = "pages/empty.html"
