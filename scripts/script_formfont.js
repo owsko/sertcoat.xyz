e7fd4cef9ec1a8b95fd6103ac5e4bee61b1de4c6
owsko <owslo.pm@gmail.com>
2024-07-11 18:58:28 +0300

    idek

--- scripts/script_formfont.js
+++ scripts/script_formfont.js
@@ -1,5 +1,6 @@
 const fontType = ["'Yarndings 20'"];
 const font_sub = document.querySelector(".form");
+const buttonEl = document.getElementById("btn");
 
 font_sub.addEventListener("submit", function randomizeFont() {
     let fontEl = document.getElementsByClassName("randomfont");
@@ -11,4 +12,12 @@ font_sub.addEventListener("submit", function randomizeFont() {
         fontEl[i].style.borderTopWidth = '0px';
         fontEl[i].style.borderBottomWidth = '0px';
     }
+});
+
+buttonEl.addEventListener("mouseover", function changeName() {
+    buttonEl.textContent = "SEND";
+});
+
+buttonEl.addEventListener("mouseout", function changeName() {
+    buttonEl.textContent = "__φ(．．)";
 });
